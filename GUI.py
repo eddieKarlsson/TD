077487e9d41f3c6a2502d9b82cbe50f017457ba9
Eddie Karlsson <60854521+eddieKarlsson@users.noreply.github.com>
2020-05-12 08:52:14 +0200

    Removed test comments

--- GUI.py
+++ GUI.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog
 import pickle
+import os
 from os import path
 
 # load user data if it exists, otherwise create dict
@@ -11,6 +12,7 @@ if path.exists('user_data.pickle'):
 else:
     user_data = {
         'excel_path': 'no file loaded',
+        'output_path': 'no path selected',
         'gen_valve': True,
         'gen_motor': True,
         'gen_ai': True,
@@ -20,31 +22,54 @@ else:
     }
 
 
-# constants
-height = 300
-width = 500
-frameColor = "#2b2b2b"
-button_bg = "#2b2b2b"
-button_fg = "#D5D5D5"  # text color
-fontSize = 10
-
-print((user_data['excel_path']))
-
 class TD_UI:
     def __init__(self, master):
+        """Constants"""
+        self.height = 300
+        self.width = 500
+        self.frameColor = "#2b2b2b"
+        self.buttonWidth = 0.18
+        self.button_bg = "#2b2b2b"
+        self.button_fg = "#FFFFFF"  # text color
+        self.fontSize = 10
+        self.buttonYSpacing = 0.12
+
+        """Create drop-down menu"""
+        self.menu = tk.Menu(master)
+        master.config(menu=self.menu)
+
+        # file submenu
+        self.subMenu = tk.Menu(self.menu)
+        self.subMenu = tk.Menu(self.menu, tearoff=0)
+        self.menu.add_cascade(label="File", menu=self.subMenu)
+        self.subMenu.add_command(label="Exit", command=master.quit)
+
+        # view submenu
+        self.viewSubMenu = tk.Menu(self.menu)
+        self.viewSubMenu = tk.Menu(self.menu, tearoff=0)
+        self.menu.add_cascade(label="View", menu=self.viewSubMenu)
+        self.viewSubMenu.add_command(label="Log file", command=self.openLogFile)
+
         """Create window"""
-        canvas = tk.Canvas(root, height=height, width=width)
+        canvas = tk.Canvas(root, height=self.height, width=self.width)
         canvas.pack()
-        frame = tk.Frame(root, bg=frameColor)
+        frame = tk.Frame(root, bg=self.frameColor)
         frame.place(relwidth=1, relheight=1)
 
         """Create window contents"""
         # Excel button
-        self.excelButton = tk.Button(root, text="Select Excel TD", bg=button_bg, fg=button_fg, command=self.browseExcel)
-        self.excelButton.place(relx=0.05, rely=0.1, relheight=0.08)
+        self.excelButton = tk.Button(root, text="Select Excel...", bg=self.button_bg, fg=self.button_fg, command=self.browseExcel)
+        self.excelButton.place(relx=0.03, rely=0.1, relheight=0.08, relwidth=self.buttonWidth)
         # Excel path label
-        self.excelLabel = tk.Label(frame, bg=button_bg, fg=button_fg, text=(user_data['excel_path']))
-        self.excelLabel.place(relx=0.28, rely=0.1, relheight=0.08)
+        self.excelLabel = tk.Label(frame, bg=self.button_bg, fg=self.button_fg, text=(user_data['excel_path']))
+        self.excelLabel.place(relx=0.23, rely=0.1, relheight=0.08)
+
+        # Output path button
+        self.outpathButton = tk.Button(root, text="Output path...", bg=self.button_bg, fg=self.button_fg, command=self.outputPath)
+        self.outpathButton.place(relx=0.03, rely=0.1 + self.buttonYSpacing, relheight=0.08, relwidth=self.buttonWidth)
+        # Output path label
+        self.outpathLabel = tk.Label(frame, bg=self.button_bg, fg=self.button_fg, text=(user_data['output_path']))
+        self.outpathLabel.place(relx=0.23, rely=0.1 + self.buttonYSpacing, relheight=0.08)
 
     def browseExcel(self):
         excelPath = filedialog.askopenfilename(filetypes=(("Excel files", "*.xlsx"), ("all files", "*.*")))
@@ -53,8 +78,18 @@ class TD_UI:
         # Update label
         self.excelLabel.config(text=excelPath)
 
-        print(user_data['excel_path'])
+    def outputPath(self):
+        output_path = filedialog.askdirectory()
+        # Write to user_data dictionary, to save it for later.
+        user_data['output_path'] = output_path
+        # Update label
+        self.outpathLabel.config(text=output_path)
 
+    def openLogFile(self):
+        os.system('log.log')
+
+    def doNothing(self):
+        print('did nothing')
 
 
 # Call
