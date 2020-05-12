1e2fd9de550c6910d3240fe3e865817b48e09770
Eddie Karlsson <60854521+eddieKarlsson@users.noreply.github.com>
2020-05-12 20:31:17 +0200

    added OutputPath and submenu

--- GUI.py
+++ GUI.py
@@ -14,18 +14,15 @@ else:
     user_data = {
         'excel_path': 'no file loaded',
         'output_path': 'no path selected',
-        'gen_valve': True,
-        'gen_motor': True,
-        'gen_ai': True,
-        'gen_ao': True,
-        'gen_di': True,
-        'gen_do': True
     }
 
+class TdUI(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
 
-class TD_UI:
-    def __init__(self, master):
-        """Constants"""
+        # Constants
         self.height = 300
         self.width = 500
         self.frameColor = "#2b2b2b"
@@ -34,72 +31,93 @@ class TD_UI:
         self.button_fg = "#FFFFFF"  # text color
         self.fontSize = 10
         self.buttonYSpacing = 0.12
+        self.checkbuttonYSpacing = 0.1
 
+        # Call app
+        self.create_window()
+        self.create_window_contents()
+        self.create_dropdown()
+
+    def create_window(self):
+        """Create window"""
+        self.canvas = tk.Canvas(self.master, height=self.height, width=self.width)
+        self.canvas.pack()
+        self.frame = tk.Frame(self.master, bg=self.frameColor)
+        self.frame.place(relwidth=1, relheight=1)
+
+    def create_dropdown(self):
         """Create drop-down menu"""
-        self.menu = tk.Menu(master)
-        master.config(menu=self.menu)
+        self.menu = tk.Menu(self.master)
+        self.master.config(menu=self.menu)
 
         # file submenu
         self.subMenu = tk.Menu(self.menu)
         self.subMenu = tk.Menu(self.menu, tearoff=0)
         self.menu.add_cascade(label="File", menu=self.subMenu)
-        self.subMenu.add_command(label="Exit", command=master.quit)
+        self.subMenu.add_command(label="Exit", command=self.master.quit)
 
         # view submenu
         self.viewSubMenu = tk.Menu(self.menu)
         self.viewSubMenu = tk.Menu(self.menu, tearoff=0)
         self.menu.add_cascade(label="View", menu=self.viewSubMenu)
-        self.viewSubMenu.add_command(label="Log file", command=self.openLogFile)
+        self.viewSubMenu.add_command(label="Log file", command=self.open_logfile)
+        self.viewSubMenu.add_command(label="Settings file", command=self.open_settings)
 
-        """Create window"""
-        canvas = tk.Canvas(root, height=self.height, width=self.width)
-        canvas.pack()
-        frame = tk.Frame(root, bg=self.frameColor)
-        frame.place(relwidth=1, relheight=1)
 
+    def create_window_contents(self):
         """Create window contents"""
         # Excel button
-        self.excelButton = tk.Button(root, text="Select Excel...", bg=self.button_bg, fg=self.button_fg, command=self.browseExcel)
+        self.excelButton = tk.Button(self.master, text="Select Excel...", bg=self.button_bg, fg=self.button_fg,
+                                     command=self.browse_excel)
         self.excelButton.place(relx=0.03, rely=0.07, relheight=0.08, relwidth=self.buttonWidth)
         # Excel path label
-        self.excelLabel = tk.Label(frame, bg=self.button_bg, fg=self.button_fg, text=(user_data['excel_path']))
+        self.excelLabel = tk.Label(self.frame, bg=self.button_bg, fg=self.button_fg, text=(user_data['excel_path']))
         self.excelLabel.place(relx=0.23, rely=0.07, relheight=0.08)
 
         # Output path button
-        self.outpathButton = tk.Button(root, text="Output path...", bg=self.button_bg, fg=self.button_fg, command=self.outputPath)
+        self.outpathButton = tk.Button(self.master, text="Output path...", bg=self.button_bg, fg=self.button_fg,
+                                       command=self.output_path)
         self.outpathButton.place(relx=0.03, rely=0.07 + self.buttonYSpacing, relheight=0.08, relwidth=self.buttonWidth)
         # Output path label
-        self.outpathLabel = tk.Label(frame, bg=self.button_bg, fg=self.button_fg, text=(user_data['output_path']))
+        self.outpathLabel = tk.Label(self.frame, bg=self.button_bg, fg=self.button_fg, text=(user_data['output_path']))
         self.outpathLabel.place(relx=0.23, rely=0.07 + self.buttonYSpacing, relheight=0.08)
 
-    def browseExcel(self):
+        # Run scripts
+        self.run_program = tk.Button(self.master, text="Run script", bg=self.button_bg, fg=self.button_fg,
+                                       command=self.run_python_windows_command)
+        self.run_program.place(relx=0.03, rely=0.8, relheight=0.08, relwidth=self.buttonWidth)
+
+
+    def browse_excel(self):
         excelPath = filedialog.askopenfilename(filetypes=(("Excel files", "*.xlsx"), ("all files", "*.*")))
         # Write to user_data dictionary, to save it for later.
         user_data['excel_path'] = excelPath
         # Update label
         self.excelLabel.config(text=excelPath)
 
-    def outputPath(self):
+    def output_path(self):
         output_path = filedialog.askdirectory()
         # Write to user_data dictionary, to save it for later.
         user_data['output_path'] = output_path
         # Update label
         self.outpathLabel.config(text=output_path)
 
-    def openLogFile(self):
+    def open_logfile(self):
         os.system('log.log')
 
-    def doNothing(self):
-        print('did nothing')
+    def open_settings(self):
+        os.system('settings.py')
 
+    def run_python_windows_command(self):
+        os.system('run.bat')
 
 
 
 # Calls
 root = tk.Tk()
 root.title('MC TD Gen')
-b = TD_UI(root)
-root.mainloop()
+app = TdUI(master=root)
+app.mainloop()
 
 # dump user data
 with open('user_data.pickle', 'wb') as f:
--- run.bat
+++ run.bat
@@ -1,2 +1,1 @@
 python Gen_TD.py
-pause
